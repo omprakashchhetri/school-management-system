c51eed50076de87b89cdddfbadd42f65a60f064a
omprakashchhetri <omprakashchhetri>
2025-08-12 01:08:57 +0530

    Views Added

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('templates/header')
+            .  view('templates/sidebar')
+            .  view('templates/topbar')
+            .  view('pages/dashboard')
+            .  view('templates/footer')
+        ;
     }
 }
